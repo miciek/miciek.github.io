bf1a42d21141e20c94d073eb12eb77ce440668e1
Michał Płachta <miciek@exul.net>
2025-03-27 08:30:01 +0100

    Add slides for the Scalar retries talk

---
+++ slides/retries/assets/md-BV5q7q_O.js
@@ -0,0 +1,8 @@
+import{g as p,h as u}from"./modules/unplugin-icons-BuEIV_o9.js";import{b as _,o as f,w as s,g as o,e as a,ad as e,i as c,v as d,x as g,D as n}from"./modules/vue-CQgeQ0W-.js";import{_ as b,a as k}from"./two-cols.vue_vue_type_script_setup_true_lang-DAQkEKq9.js";import{u as h,f as y}from"./slidev/context-BY4Txrd-.js";import"./index-DjcPfP7W.js";import"./modules/shiki-BEsVBpME.js";const $={__name:"slides.md__slidev_44",setup(x){const{$clicksContext:l,$frontmatter:r}=h();return l.setup(),(P,t)=>{const m=p,i=u;return f(),_(k,d(g(n(y)(n(r),43))),{right:s(T=>[t[4]||(t[4]=o("img",{class:"absolute top-10 right-10 size-120",alt:"",src:b},null,-1)),c(`
+
+TODOs:
+- jitter and co
+- RT mentions? they are in the abstract
+- lazy list: look at me, I am retry policy rn meme?
+- rename blueprints to schedules?
+`)]),default:s(()=>[t[5]||(t[5]=o("h1",null,"Thank you",-1)),t[6]||(t[6]=o("h2",null,"Michał Płachta",-1)),o("ul",null,[o("li",null,[a(m),t[0]||(t[0]=e(" bluesky ")),t[1]||(t[1]=o("a",{href:"https://bsky.app/profile/mplachta.com",target:"_blank"},"@mplachta.com",-1))]),o("li",null,[a(i),t[2]||(t[2]=e(" blog ")),t[3]||(t[3]=o("a",{href:"https://michalplachta.com",target:"_blank"},"michalplachta.com",-1))])])]),_:1},16)}}};export{$ as default};
---
+++ slides/retries/assets/md-XbysNLl9.js
@@ -0,0 +1,19 @@
+import{_ as r}from"./slidev/CodeBlockWrapper.vue_vue_type_script_setup_true_lang-BiEvtvP6.js";import{b as c,o as d,w as l,g as s,e as u,m as o,ad as e,v as m,x as f,D as a}from"./modules/vue-CQgeQ0W-.js";import{I as y}from"./slidev/default-CsN6QSls.js";import{u as I,f as g}from"./slidev/context-BY4Txrd-.js";import"./modules/unplugin-icons-BuEIV_o9.js";import"./index-DjcPfP7W.js";import"./modules/shiki-BEsVBpME.js";const x={__name:"slides.md__slidev_18",setup(w){const{$clicksContext:i,$frontmatter:t}=I();return i.setup(),(R,n)=>{const p=r;return d(),c(y,m(f(a(g)(a(t),17))),{default:l(()=>[n[1]||(n[1]=s("h1",null,"Running the test",-1)),u(p,o({},{ranges:[]}),{default:l(()=>n[0]||(n[0]=[s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-text"},[s("span",{class:"line"},[s("span",null,"FAIL: I will succeed after 5s! Only 0ms passed. Retrying...")]),e(`
+`),s("span",{class:"line"},[s("span",null,"FAIL: I will succeed after 5s! Only 5ms passed. Retrying...")]),e(`
+`),s("span",{class:"line"},[s("span",null,"FAIL: I will succeed after 5s! Only 10ms passed. Retrying...")]),e(`
+`),s("span",{class:"line"},[s("span",null,"FAIL: I will succeed after 5s! Only 20ms passed. Retrying...")]),e(`
+`),s("span",{class:"line"},[s("span",null,"FAIL: I will succeed after 5s Only 35ms passed. Retrying...")]),e(`
+`),s("span",{class:"line"},[s("span",null,"FAIL: I will succeed after 5s! Only 60ms passed. Retrying...")]),e(`
+`),s("span",{class:"line"},[s("span",null,"FAIL: I will succeed after 5s! Only 100ms passed. Retrying...")]),e(`
+`),s("span",{class:"line"},[s("span",null,"FAIL: I will succeed after 5s! Only 165ms passed. Retrying...")]),e(`
+`),s("span",{class:"line"},[s("span",null,"FAIL: I will succeed after 5s! Only 270ms passed. Retrying...")]),e(`
+`),s("span",{class:"line"},[s("span",null,"FAIL: I will succeed after 5s! Only 440ms passed. Retrying...")]),e(`
+`),s("span",{class:"line"},[s("span",null,"FAIL: I will succeed after 5s! Only 715ms passed. Retrying...")]),e(`
+`),s("span",{class:"line"},[s("span",null,"FAIL: I will succeed after 5s! Only 1160ms passed. Retrying...")]),e(`
+`),s("span",{class:"line"},[s("span",null,"FAIL: I will succeed after 5s! Only 1860ms passed. Retrying...")]),e(`
+`),s("span",{class:"line"},[s("span",null,"FAIL: I will succeed after 5s! Only 2560ms passed. Retrying...")]),e(`
+`),s("span",{class:"line"},[s("span",null,"FAIL: I will succeed after 5s! Only 3260ms passed. Retrying...")]),e(`
+`),s("span",{class:"line"},[s("span",null,"FAIL: I will succeed after 5s! Only 3960ms passed. Retrying...")]),e(`
+`),s("span",{class:"line"},[s("span",null,"FAIL: I will succeed after 5s! Only 4660ms passed. Retrying...")]),e(`
+`),s("span",{class:"line"},[s("span",null,"[info] FunctionalRetriesImperativelyTest")]),e(`
+`),s("span",{class:"line"},[s("span",null,"[info] + retry with the following strategy: 5ms fib backoff, capped at 700ms, max 5s 38ms")])])],-1)])),_:1},16)]),_:1},16)}}};export{x as default};
